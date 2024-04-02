242bd2450581a1e702bdc32a093f3882c9daf9ee
Saimone2 <hav1802200474@gmail.com>
2024-04-02 15:21:03 +0300

    init

---
+++ src/test/java/com/saimone/bvp_software_task/BvpSoftwareTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saimone.bvp_software_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvpSoftwareTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
